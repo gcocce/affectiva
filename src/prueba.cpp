7d9cb227955c4664fd07ed2648e4afbe560d5db9
Gaston Cocce <gcocce@gmail.com>
2017-11-30 15:17:32 +0000

    Se muestra por pantallos datos de emociones, expresiones y apariencia

--- src/prueba.cpp
+++ src/prueba.cpp
@@ -52,28 +52,51 @@ public:
 	
 	void onImageResults(std::map<FaceId, Face> faces, Frame image){
 		LOG(plog::debug) << "onImageResults... faces.size():" << faces.size();
-		std::printf("onImageResults... faces.size():%l\n", faces.size());
+		std::printf("onImageResults... faces.size():%d\n", faces.size());
 		
 		// Log content:
 		for (std::map<FaceId, Face>::iterator it=faces.begin(); it!=faces.end(); ++it){
-			LOG(plog::debug) << "FaceId: " << it->first << "FaceId: " << "\n";
-			//std::cout << it->first << " => " << it->second << '\n';
-		}
+			LOG(plog::debug) << "FaceId: " << it->first << endl;
 		
-		if(mfaceId != -1){
+			Face face=it->second;
 			
-			Face face = faces[mfaceId];
-						
-			LOG(plog::debug) << "onImageCapture... ";
+			std::printf("FaceId:%d\n", it->first);
 			
-			std::printf("smile:%f\n", face.expressions.smile);
+			// Expresiones detectables
+			std::printf("\nExpresiones:\n");
+			std::printf("Sonrisa:%.2f\t", face.expressions.smile);
+			std::printf("SonrisaFalsa:%.2f\t", face.expressions.smirk);
+			std::printf("BocaAbierta:%.2f\t", face.expressions.mouthOpen);
+			std::printf("LabiosApredados:%.2f\n", face.expressions.lipPress);
+
+			std::printf("EntreCejasArriba:%.2f\t", face.expressions.innerBrowRaise);
+			std::printf("CejasArriba:%.2f\t", face.expressions.browRaise);
+			std::printf("CejasFruncidas:%.2f\t", face.expressions.browFurrow);
+			std::printf("NarizArrugada:%.2f\n", face.expressions.noseWrinkle);
+
+			std::printf("OjosCerrados:%.2f\t", face.expressions.eyeClosure);
+			std::printf("OjosMuyAbierto:%.2f\t", face.expressions.eyeWiden);
+			std::printf("Atenciòn:%.2f\t", face.expressions.attention);			
+			std::printf("MandìbulaCaida:%.2f\n", face.expressions.jawDrop);
 			
-			std::printf("mouthopen:%f\n", face.expressions.mouthOpen);
+			// Emociones detectables
+			std::printf("\nEmociones:\n");
+			std::printf("Alegrìa:%.2f\t", face.emotions.joy);
+			std::printf("Miedo:%.2f\t", face.emotions.fear);
+			std::printf("Disgusto:%.2f\t", face.emotions.disgust);
+			std::printf("Tristeza:%.2f\t", face.emotions.sadness);
 			
-			//std::printf("sadness:%f\n", face.expressions.sadness);
+			std::printf("Enojo:%.2f\n", face.emotions.anger);
+			std::printf("Sorpresa:%.2f\t", face.emotions.surprise);
+			std::printf("Positividad:%.2f\t", face.emotions.valence);
+			std::printf("Desprecio:%.2f\n", face.emotions.contempt);			
 			
-			//face.emotions.surprise
-			std::printf("surprise:%f\n", face.emotions.surprise);
+			// Apariencias detectables
+			std::printf("\nApariencia\n");
+			std::printf("Lentes: %d\t", face.appearance.glasses);
+			std::printf("Sexo: %d\t", face.appearance.gender);
+			std::printf("Edad: %d\t", face.appearance.age);
+			std::printf("Etnia: %d\n", face.appearance.ethnicity);
 		}
 		
 		std::printf("Image Results :\n");	
